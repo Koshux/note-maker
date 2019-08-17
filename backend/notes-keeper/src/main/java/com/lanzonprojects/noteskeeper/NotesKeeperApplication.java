3bc049e5a1c871e3aa7d4d26eed3edf2a7ea2c21
dynamo-whiz <dynamowhiz@gmail.com>
2019-08-17 14:02:57 +0200

    Created structure for the note-maker tool
    1. Initialised frontend application.
     - ReactJS `create-react-app` to provide boilerplate structure including possibly redundant files (serves as guidance)
     - webpack-dev-server for auto-reload
    
    2, Intialised backend spring-boot application.
     - Spring Boot shell without any further dependencies.

---
+++ backend/notes-keeper/src/main/java/com/lanzonprojects/noteskeeper/NotesKeeperApplication.java
@@ -0,0 +1,13 @@
+package com.lanzonprojects.noteskeeper;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NotesKeeperApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NotesKeeperApplication.class, args);
+	}
+
+}
