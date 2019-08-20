bddf0e8fa159f7c4e020f778fef5e01b5377cacf
koshux <lanzonprojects@gmail.com>
2019-08-21 01:04:11 +0200

    Renamed datatable component
    Renamed the `DataTable` component to `note-datatable` for consistent
    naming.

--- frontend/notes-keeper/src/components/notes-datatable.js
+++ frontend/notes-keeper/src/components/notes-datatable.js
@@ -1,19 +1,21 @@
+import moment from 'moment'
 import React, { forwardRef } from 'react'
 import MaterialTable from 'material-table'
 import LastPage from '@material-ui/icons/LastPage'
 import FirstPage from '@material-ui/icons/FirstPage'
+import ArrowUpward from '@material-ui/icons/ArrowUpward'
 import ChevronLeft from '@material-ui/icons/ChevronLeft'
 import ChevronRight from '@material-ui/icons/ChevronRight'
 
-export default function MaterialTableDemo() {
+export default function NotesDataTable(props) {
   // TODO: Use this.props.notes to load remote notes
   // TODO: Ensure that once a new note has been added, it updates the table.
   // TODO: Ensure that pagination works well with JSON:API (fetch remote data).
-  console.log(this)
+  // console.log(props.notes)
   const [state, setState] = React.useState({
     columns: [
       { title: 'Description', field: 'description', sorting: false  },
-      { title: 'Creation Date', field: 'creationDate', type: 'date' },
+      { title: 'Creation Date', field: 'creationDate', type: 'datetime' },
     ],
     data: [
       { description: 'Mehmet', creationDate: new Date() },
@@ -32,21 +34,29 @@ export default function MaterialTableDemo() {
   })
 
   const tableIcons = {
-    FirstPage: forwardRef((props, ref) => <FirstPage {...props} ref={ref} />),
-    LastPage: forwardRef((props, ref) => <LastPage {...props} ref={ref} />),
-    NextPage: forwardRef((props, ref) => <ChevronRight {...props} ref={ref} />),
-    PreviousPage: forwardRef((props, ref) => <ChevronLeft {...props} ref={ref} />)
+    FirstPage:
+      forwardRef((props, ref) => <FirstPage {...props} ref={ref} />),
+    LastPage:
+      forwardRef((props, ref) => <LastPage {...props} ref={ref} />),
+    NextPage:
+      forwardRef((props, ref) => <ChevronRight {...props} ref={ref} />),
+    PreviousPage:
+      forwardRef((props, ref) => <ChevronLeft {...props} ref={ref} />),
+    SortArrow:
+      forwardRef((props, ref) => <ArrowUpward {...props} ref={ref} />)
   }
 
   return (
     <MaterialTable
       title="Notes"
+      // data={state.data}
       data={state.data}
       icons={tableIcons}
       columns={state.columns}
       options={{
         add: false,
         pageSize: 10,
+        update: false,
         search: false,
         pageSizeOptions: [10]
       }}
