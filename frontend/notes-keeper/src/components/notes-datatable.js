8269ab8aa69f63ab74b9ad7118a0f18a9ecce2ce
koshux <lanzonprojects@gmail.com>
2019-08-21 02:12:06 +0200

    Changed `material-table` to use remote data
    1. Implemented sorting & pagination.
    2. Removed mock data.
    3. Request API with the expected JSON:API format.
    4. Implemented error handling.
    5. Removed unused `setState` for now.

--- frontend/notes-keeper/src/components/notes-datatable.js
+++ frontend/notes-keeper/src/components/notes-datatable.js
@@ -12,25 +12,11 @@ export default function NotesDataTable(props) {
   // TODO: Ensure that once a new note has been added, it updates the table.
   // TODO: Ensure that pagination works well with JSON:API (fetch remote data).
   // console.log(props.notes)
-  const [state, setState] = React.useState({
+  const [state] = React.useState({
     columns: [
       { title: 'Description', field: 'description', sorting: false  },
       { title: 'Creation Date', field: 'creationDate', type: 'datetime' },
-    ],
-    data: [
-      { description: 'Mehmet', creationDate: new Date() },
-      { description: 'Mehmet', creationDate: new Date() },
-      { description: 'Mehmet', creationDate: new Date() },
-      { description: 'Mehmet', creationDate: new Date() },
-      { description: 'Mehmet', creationDate: new Date() },
-      { description: 'Mehmet', creationDate: new Date() },
-      { description: 'Mehmet', creationDate: new Date() },
-      { description: 'Mehmet', creationDate: new Date() },
-      { description: 'Mehmet', creationDate: new Date() },
-      { description: 'Mehmet', creationDate: new Date() },
-      { description: 'Mehmet', creationDate: new Date() },
-      { description: 'Mehmet', creationDate: new Date() }
-    ],
+    ]
   })
 
   const tableIcons = {
@@ -46,11 +32,15 @@ export default function NotesDataTable(props) {
       forwardRef((props, ref) => <ArrowUpward {...props} ref={ref} />)
   }
 
+  const headers = {
+    method: 'GET',
+    cache: 'no-cache',
+    headers: { 'Content-Type': 'application/vnd.api+json' }
+  }
+
   return (
     <MaterialTable
       title="Notes"
-      // data={state.data}
-      data={state.data}
       icons={tableIcons}
       columns={state.columns}
       options={{
@@ -60,6 +50,47 @@ export default function NotesDataTable(props) {
         search: false,
         pageSizeOptions: [10]
       }}
+      data={query => new Promise((resolve, reject) => {
+        const offset = query.page === 0 ? 0 : query.page + query.pageSize
+        const limit = query.page + query.pageSize
+
+        let url = 'lanzonprojects/api/notes'
+        url += `?page[offset]=${offset}`
+        url += `&page[limit]=${limit}`
+
+        fetch(url, headers)
+          .then(response => response.json())
+          .then(result => {
+            console.log(result)
+            // Return the promise to prevent further execution.
+            if (result == null || result.errors != null) {
+              return resolve({
+                data: [],
+                page: 0,
+                totalCount: 0
+              })
+            }
+
+            const notes = result.data.reduce((notes, { attributes }) => {
+              notes.push({
+                description: attributes.description,
+                creationDate: moment(attributes.creationDate).format('LLL')
+              })
+
+              return notes
+            }, [])
+
+            // API must resolve to load notes.
+            return resolve({
+              data: notes,
+              page: query.page,
+              totalCount: result.meta.totalResourceCount
+            })
+          }).catch(err => {
+            console.error(err)
+            return reject(err)
+          })
+      })}
     />
   )
 }
