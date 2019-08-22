95fb2db58e9e3b0cfecd6b5b24a80ff929df26fe
koshux <lanzonprojects@gmail.com>
2019-08-22 02:47:49 +0200

    Cleaned up entry point
    Cleaned up redundant code from the app entry point.

--- frontend/notes-keeper/src/App.js
+++ frontend/notes-keeper/src/App.js
@@ -1,32 +1,14 @@
-// import logo from './logo.svg';
+import React from 'react'
 import IndexPage from './pages/index'
-import React from 'react';
 
 class App extends React.PureComponent {
-  state = {
-    notes: {
-      1: {
-        _id: 1,
-        title: 'Hello World!',
-        description: 'First note.',
-        creationDate: new Date()
-      },
-      2: {
-        _id: 1,
-        title: 'Hello World!',
-        description: 'First note.',
-        creationDate: new Date()
-      }
-    }
-  }
-
   render () {
     return (
       <div className="app">
-        <IndexPage notes={this.state.notes} />
+        <IndexPage />
       </div>
-    );
+    )
   }
 }
 
-export default App;
+export default App
